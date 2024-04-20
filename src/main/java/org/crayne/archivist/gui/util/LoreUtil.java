97b4d3ee471f6bf8547f884fa66ba0b375726c79
crxyne <craynemail@gmail.com>
2024-04-21 00:11:20 +0200

    cleaned up lore util

--- src/main/java/org/crayne/archivist/gui/util/LoreUtil.java
+++ src/main/java/org/crayne/archivist/gui/util/LoreUtil.java
@@ -3,6 +3,7 @@ package org.crayne.archivist.gui.util;
 import org.crayne.archivist.inventory.ArchivistInventory;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
 import java.util.List;
 
 public class LoreUtil {
@@ -12,12 +13,16 @@ public class LoreUtil {
     }
 
 
-    public static void addRemainingLines(@NotNull final List<String> lore, @NotNull final List<String> add) {
-        for (int i = 0; i < 8 && i < add.size(); i++) {
-            lore.add(ArchivistInventory.secondaryText(add.get(i)).legacyText());
+    public static void addRemainingLines(@NotNull final List<String> lore, @NotNull final Collection<String> add) {
+        int i = 0;
+        for (final String line : add) {
+            if (i >= 8) break;
+
+            lore.add(ArchivistInventory.secondaryText(line).legacyText());
+            i++;
         }
         final int remaining = add.size() - 8;
-        if (remaining > 0) lore.add(ArchivistInventory.mainText("...and " + remaining + "more").legacyText());
+        if (remaining > 0) lore.add(ArchivistInventory.mainText("...and " + remaining + " more").legacyText());
     }
 
 }
